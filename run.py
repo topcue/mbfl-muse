a45eaa32b2bfa5571e60c6e1d1e37ca1f69fc3ea
topcue <topcue1019@naver.com>
2023-06-03 14:31:13 +0900

    Generalize by implementing as a class

--- run.py
+++ run.py
@@ -4,56 +4,13 @@ import sys
 import json
 import subprocess
 
+##! ==================== utils ====================
 def get_file_names(dir_path):
-  file_names = []
-  for path in os.listdir(dir_path):
-    if os.path.isfile(os.path.join(dir_path, path)):
-      file_names.append(path)
-  return sorted(file_names)
-
-
-def build_put(target):
-    cmd = ""
-    cmd += "clang-12 -fexperimental-new-pass-manager "
-    cmd += "-fpass-plugin=/usr/lib/mull-ir-frontend-12 "
-    cmd += "-g -grecord-command-line "
-    cmd += "-fprofile-instr-generate -fcoverage-mapping "
-    cmd += "%s.c -o %s" % (target, target)
-    print("[DEBUG] cmd:", cmd)
-    os.system(cmd)
-
-
-##! deprecated
-# ##! TODO: Fix this dump way in single run
-# def generate_mutants_old(target: str, report_name: str, args: str):
-#     ##! generate patches
-#     cmd = ""
-#     cmd += "mull-runner-12 --reporters=Patches --report-dir=reports "
-#     cmd += "--report-name=%s %s " % (report_name, target)
-#     cmd += "-test-program=python3 -- test_helper.py ./%s %s" % (target, args)
-#     print("[DEBUG] cmd:", cmd)
-#     os.system(cmd)
-
-#     ##! create json file
-#     cmd = ""
-#     cmd += "mull-runner-12 --reporters=Elements --report-dir=reports "
-#     cmd += "--report-name=%s %s " % (report_name, target)
-#     cmd += "-test-program=python3 -- test_helper.py ./%s %s" % (target, args)
-#     print("[DEBUG] cmd:", cmd)
-#     os.system(cmd)
-#     os.system("rm reports/%s.html" % (report_name))
-
-
-def generate_patches(target: str, report_name: str, args: str):
-    ##! generate patches
-    cmd = ""
-    cmd += "mull-runner-12 --reporters=Patches --reporters=Elements "
-    cmd += "--ide-reporter-show-killed " ##! for debugging
-    cmd += "--report-dir=reports --report-name=%s %s " % (report_name, target)
-    cmd += "-test-program=python3 -- test_helper.py ./%s %s" % (target, args)
-    print("[DEBUG] cmd:", cmd)
-    os.system(cmd)
-    os.system("rm reports/%s.html" % (report_name))
+    file_names = []
+    for path in os.listdir(dir_path):
+        if os.path.isfile(os.path.join(dir_path, path)):
+            file_names.append(path)
+    return file_names
 
 
 ##! TODO: Fix me
@@ -61,104 +18,127 @@ def set_args_for_max(args: tuple):
     return " ".join(map(str, args))
 
 
-# ##! read json to judge P/F
-# def foo(report_name):
-#     file_path = "reports/%s.json" % report_name
-
-#     with open(file_path, "r") as file:
-#         data = json.load(file)
-
-#     files = data["files"]
-#     for file_path, file_data in files.items():
-#         mutants = file_data["mutants"]
-#         for mutant in mutants:
-#             line_number = mutant["location"]["start"]["line"]
-#             status = mutant["status"]
-#             print(f"line: {line_number}, Status: {status}")
-
-
 def pathces_line_number(file_name):
     pattern = r"-L(\d+)-"
     match = re.search(pattern, file_name)
     return match.group(1)
 
+##! ==============================================
 
-def patch_and_build(target, report_name):
-    ##! for test
-    os.system("rm -rf ./mutants && mkdir -p mutants")
 
-    ##! mu0: original code
-    os.system("cp %s.c mutants/mu0.c" % (target))
-    os.system("cp oracle_%s.c mutants/" % (target))
-    ##! build mu0
-    os.system("clang-12 mutants/mu0.c -o mutants/mu0")
+class Muse():
+    target = ""
+    target_path = ""
+    target_dir_path = ""
 
-    patches_path = "reports/%s-patches" % (report_name)
-    file_names = get_file_names(patches_path)
-    sorted_file_names = sorted(file_names, key=pathces_line_number)
-    for idx, file_name in enumerate(sorted_file_names):
-        os.system("cp %s mutants/" % (os.path.join(patches_path, file_name)))
+    def __init__(self, target, tc_list):
+        self.target = target
+        self.target_dir_path = os.path.join("targets", target)
+        self.target_path = os.path.join("targets", target, target)
+        self.tc_list = tc_list
+
+
+    def build_put(self):
+        target_source_path = os.path.join(self.target_dir_path, self.target)
+        cmd = ""
+        cmd += "clang-12 -fexperimental-new-pass-manager "
+        cmd += "-fpass-plugin=/usr/lib/mull-ir-frontend-12 "
+        cmd += "-g -grecord-command-line "
+        cmd += "-fprofile-instr-generate -fcoverage-mapping "
+        cmd += "%s.c -o %s.exec" % (target_source_path, target_source_path)
+        # print("[DEBUG] cmd:", cmd)
+        os.system(cmd)
+
+
+    def generate_patches(self, report_name: str, args: str):
+        report_path = os.path.join(self.target_dir_path, "reports", report_name)
+        target_bin_path = os.path.join(self.target_dir_path, self.target + ".exec")
+
+        # os.system("cp /home/topcue/mbfl-muse/mull.yml /home/topcue/mbfl-muse/targets/max/")
+        ##! generate patches
+        cmd = ""
+        cmd += "mull-runner-12 --reporters=Patches --reporters=Elements "
+        cmd += "--ide-reporter-show-killed " ##! for debugging
+        cmd += "--report-name=%s --report-dir=%s %s " % (report_name, report_path, target_bin_path)
+        cmd += "-test-program=python3 -- test_helper.py ./%s %s" % (target_bin_path, args)
+        # print("[DEBUG] cmd:", cmd)
+        os.system(cmd)
         
-        line_num = pathces_line_number(file_name)
-
-        ##! patch
-        mutant_name = "mu%s-L%s" % (idx+1, line_num)
-        cmd_patch = "cd mutants; patch < %s mu0.c -o %s.c" % (file_name, mutant_name)
-        # print("[DEBUG] cmd:", cmd_patch)
-        os.system(cmd_patch)
-        
-        ##! TODO: Fix me
-        ##! manually patch (resolve mull bug)
-        os.system("cp mu1-L6.c mutants/mu1-L6.c")
-        os.system("rm mutants/%s" % (file_name))
-
-        ##! build mutant
-        os.system("cd mutants; clang-12 %s.c -o %s" % (mutant_name, mutant_name))
+        ##! clean up
+        os.system("rm %s.html" % os.path.join(report_path, report_name))
 
 
-def tmp(target, args_list):
-    tmp_mutatns = get_file_names("mutants")
-    mutants = [ x for x in tmp_mutatns if not x.endswith(".c") ]
-    mutants.remove("mu0")
-    
-    for idx_args, args in enumerate(args_list):
-        ##! test mu0
-        binary_path = "./mutants/mu0"
-        args_ = [str(args[0]), str(args[1])]
-        result = subprocess.run([binary_path] + args_, capture_output=True)
-        mu0_res = "P" if result.returncode == 0 else "F"
-        print("[*] TC:", args)
+    def dry_run(self):
+        ##! generate reports dir
+        reports_path = os.path.join(self.target_dir_path, "reports")
+        os.system("rm -rf %s && mkdir -p %s" % (reports_path, reports_path))
 
-        ##! test mutants
-        for idx_mu, mutant in enumerate(mutants):
-            binary_path = "./mutants/%s" % (mutant)
-            args_ = [str(args[0]), str(args[1])]
-            result = subprocess.run([binary_path] + args_, capture_output=True)
-            ret = "P" if result.returncode == 0 else "F"
-            print("  %s: " % (mutant), end="")
-            if ret != mu0_res:
-                print("%s->%s" % (mu0_res, ret))
-            else:
-                print()
-            # print("[+] (TC, result) = (%s, %s))" % (args, ret))
+        tcs = self.tc_list
+        tc_for_dry_run = set_args_for_max(tcs[0])
+        report_name = "dry_run"
+        self.generate_patches(report_name, tc_for_dry_run)
+
+
+    def patch_and_build(self):
+        target = self.target
+        target_path = self.target_path
+
+        ##! generate mutants dir
+        mutants_path = os.path.join(self.target_dir_path, "mutatns")
+        os.system("rm -rf %s && mkdir -p %s" % (mutants_path, mutants_path))
+
+        ##! build mu0 (mu0: original code)
+        mu0_path = os.path.join(mutants_path, "mu0")
+        os.system("cp %s.c %s.c" % (target_path, mu0_path))
+        os.system("cp %s/oracle_%s.c %s" % (self.target_dir_path, target, mutants_path))
+        os.system("clang-12 %s.c -o %s.exec" % (mu0_path, mu0_path))
+
+        ##! build mutants
+        patches_dir_path = os.path.join(self.target_dir_path, "reports/dry_run/dry_run-patches") 
+        file_names = get_file_names(patches_dir_path)
+        sorted_file_names = sorted(file_names, key=pathces_line_number)
+        for idx, file_name in enumerate(sorted_file_names):
+            os.system("cp %s %s/" % (os.path.join(patches_dir_path, file_name), mutants_path))
+            line_num = pathces_line_number(file_name)
+
+            ##! patch
+            mutant_name = "mu%s-L%s" % (idx+1, line_num)
+            cmd_patch = ""
+            cmd_patch += "cd %s; " % (mutants_path)
+            cmd_patch += "patch < %s mu0.c -o %s.c" % (file_name, mutant_name)
+            # print("[DEBUG] cmd:", cmd_patch)
+            os.system(cmd_patch)
+            os.system("rm %s" % (os.path.join(mutants_path, file_name)))
+            
+            ##! build mutant
+            cmd_compile = ""
+            cmd_compile += "cd %s; " % (mutants_path)
+            cmd_compile += "clang-12 %s.c -o %s.exec" % (mutant_name, mutant_name)
+            os.system(cmd_compile)
+
+
+
 
 
 ##! TODO: Fix me
 def test_max():
+    ##! 0) set target and target path
     target = "max"
-    args_list = [(3, 1), (5, -4), (0,-4), (0,7), (-1,3)]
+    max_tc_list = [(3, 1), (5, -4), (0,-4), (0,7), (-1,3)]
+    muse_max = Muse(target, max_tc_list)
+    
 
-    ##! build put w/ mull-lib
-    build_put(target)
+    ##! 1) build put w/ mull-lib
+    muse_max.build_put()
 
-    ##! generate patches for mutants
-    dry_run_args = set_args_for_max(args_list[0])
-    report_name = "TC0"
-    generate_patches(target, report_name, dry_run_args)
+    ##! 2) generate patches for mutants
+    muse_max.dry_run()
 
-    ##! build w/ patches
-    patch_and_build(target, report_name)
-    tmp(target, args_list)
+    ##! 3) build w/ patches
+    muse_max.patch_and_build()
+
+
+    # tmp(target, args_list)
 
 
 def main():
