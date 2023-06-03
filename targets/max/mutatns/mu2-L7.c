27977d60b5dbfb8f629363416553fdca46fab279
topcue <topcue1019@naver.com>
2023-06-03 14:40:02 +0900

    Clean up

---
+++ targets/max/mutatns/mu2-L7.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "oracle_max.c"
+
+int setmax(int x, int y) {
+    int max = -x; // should be 'max = x;'
+    if (max >= y) {
+        max = y;
+        if (x * y < 0) {
+            printf("diff.sign\n");
+        }
+    }
+    printf("max: %d\n", max);
+    return max;
+}
+
+int main(int argc, char* argv[]) {
+    int x, y;
+    init_max(argc, argv, &x, &y);
+
+    printf("[*] (x, y) = (%d, %d)\n", x, y);
+
+    int max = setmax(x, y);
+    
+    return oracle_max(x, y, max);
+}
+
+// EOF
