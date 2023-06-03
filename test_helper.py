312b430292f1049887d891f231db1210d4675d16
topcue <topcue1019@naver.com>
2023-06-03 18:43:28 +0900

    Generalize test_helper.py

--- test_helper.py
+++ test_helper.py
@@ -3,14 +3,11 @@ import subprocess
 
 ##! https://mull.readthedocs.io/en/0.20.0/tutorials/NonStandardTestSuite.html#tests-in-interpreted-languages
 
-test_executable = sys.argv[1]
 
-##! TODO: Generalize me
-# test_arguments = " ".join(sys.argv[2:])
-arg1 = sys.argv[2]
-arg2 = sys.argv[3]
+cmd = sys.argv[1:]
 
-print("[DEBUG] test_helper: %s %s %s" % (test_executable, arg1, arg2))
-subprocess.run([test_executable, arg1, arg2], check=True)
+print("[DEBUG] test_helper: %s" % (" ".join(cmd)))
+
+subprocess.run(cmd, check=True)
 
 # EOF
