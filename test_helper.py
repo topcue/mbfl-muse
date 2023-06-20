14e06dee3fec79d812727af9de391838aa8681be
topcue <topcue1019@naver.com>
2023-06-20 14:23:51 +0900

    Clean up test_helper

--- test_helper.py
+++ test_helper.py
@@ -1,13 +1,9 @@
 import sys
 import subprocess
 
-##! https://mull.readthedocs.io/en/0.20.0/tutorials/NonStandardTestSuite.html#tests-in-interpreted-languages
-
-
 cmd = sys.argv[1:]
-
-print("[DEBUG] test_helper: %s" % (" ".join(cmd)))
-
 subprocess.run(cmd, check=True)
+
+# print("[DEBUG] test_helper: %s" % (" ".join(cmd)))
 
 # EOF
